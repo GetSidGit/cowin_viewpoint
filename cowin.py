1c40987888dbd7d757806395ca2e0c00641f30fa
getsidgit <siddardh.siddu@gmail.com>
2021-06-01 22:33:24 +0530

    Updated with : Gmail module

--- cowin.py
+++ cowin.py
@@ -5,6 +5,7 @@ import sys
 import time
 import urllib.request
 from twilio.rest import Client
+import smtplib
 
 def write_runtime_message(log_file_path, text):
     with open(log_file_path + "cowin_crawler.log", 'a') as data:
@@ -31,9 +32,41 @@ def trigger_message_alert(user_number, alert_message, time_delay, cowin_log_path
     write_runtime_message(cowin_log_path, "Alert sent successfully with below message : ")
     write_runtime_message(cowin_log_path, alert_message)
 
+def trigger_gmail_alert(gmail_list, alert_message, cowin_log_path):
+    gmail_string = gmail_list[len(gmail_list)-1]
+    mail_subject = alert_message.split(" For Dosage :")[0]
+    source_gmail_id = gmail_string.split("|")[0]
+    source_gmail_pwd = gmail_string.split("|")[1]
+    try:
+        source_gmail_smtp_port = gmail_string.split("|")[2]
+    except ValueError:
+        print("SMTP port must be an integer ! trying with default port 465")
+        source_gmail_smtp_port = 465
+    try:
+        gmail_handler = smtplib.SMTP_SSL("smtp.gmail.com", source_gmail_smtp_port)
+        gmail_handler.login(source_gmail_id, source_gmail_pwd)
+    except:
+        print("GMAIL Login Failed !")
+        sys.exit()
+
+    for mail_id in gmail_list[:-1]:
+        mail_data = "\r\n".join([
+            "From: " + source_gmail_id,
+            "To: " + mail_id,
+            "Subject: " + mail_subject,
+            "", alert_message])
+        try:
+            gmail_handler.sendmail(source_gmail_id, mail_id, mail_data)
+        except:
+            print("Send Mail failed ! ")
+            sys.exit()
+        write_runtime_message(cowin_log_path, "Mail sent to : " + mail_id)
+
+    gmail_handler.quit()
+
 
 def print_message(session_dict, pincode, date, hit_dosage, hit_above_18, mobile_numbers, cowin_log_path,
-                  user_twilio_string):
+                  user_twilio_string, gmail_list):
     if hit_above_18:
         age_string = "18 plus"
     else:
@@ -49,10 +82,12 @@ def print_message(session_dict, pincode, date, hit_dosage, hit_above_18, mobile_
                     "Price : " + str(session_dict["fee"]) + " INR" + "\n" + \
                     "        --- @getsidgit "
     for number in mobile_numbers:
-        if user_twilio_string is not None or user_twilio_string != '':
+        if user_twilio_string is not None and user_twilio_string != '':
             trigger_message_alert_with_twilio(number, alert_message, user_twilio_string, cowin_log_path)
         else:
             trigger_message_alert(number, alert_message, 80, cowin_log_path)
+    if gmail_list is not None:
+        trigger_gmail_alert(gmail_list, alert_message, cowin_log_path)
 
 
 def place_request(endpoint, set_ssl_context, cowin_log_path):
@@ -92,29 +127,29 @@ def refresh_codes(main_state, state_endpoint, district_endpoint_base, set_ssl_co
 
 
 def hit_handler(pincode, search_date, dosage_count, hits_dict, session_entry, user_above_18, mobile_numbers,
-                cowin_log_path, user_twilio_string):
+                cowin_log_path, user_twilio_string, gmail_list):
     if pincode + search_date + str(session_entry["center_id"]) not in hits_dict.keys():
         print_message(session_entry, pincode, search_date, dosage_count, user_above_18, mobile_numbers, cowin_log_path,
-                      user_twilio_string)
+                      user_twilio_string, gmail_list)
         hits_dict[pincode + search_date + str(session_entry["center_id"])] = "Place Holder"
 
 
 def initiate_hit_handler(district_session_entry, search_dosage, search_pincodes, pin_flag, search_date, hit_dict,
-                         search_18_flag, search_alert_numbers, cowin_log_path, user_twilio_string):
+                         search_18_flag, search_alert_numbers, cowin_log_path, user_twilio_string, gmail_list):
     if district_session_entry["available_capacity_dose" + str(search_dosage)] > 2:
         if pin_flag == 1:
             hit_handler(str(district_session_entry["pincode"]), search_date, search_dosage, hit_dict,
                         district_session_entry,
-                        search_18_flag, search_alert_numbers, cowin_log_path, user_twilio_string)
+                        search_18_flag, search_alert_numbers, cowin_log_path, user_twilio_string, gmail_list)
         if str(district_session_entry["pincode"]) in search_pincodes:
             hit_handler(str(district_session_entry["pincode"]), search_date, search_dosage, hit_dict,
                         district_session_entry,
-                        search_18_flag, search_alert_numbers, cowin_log_path, user_twilio_string)
+                        search_18_flag, search_alert_numbers, cowin_log_path, user_twilio_string, gmail_list)
 
 
 def cowin_search(endpoint_base, districts, user_pincodes, search_date, above_18_flag, user_dosages, sleep,
                  user_mobile_numbers, district_lookup, attempt_reference_dict, request_ssl_context, cowin_log_path,
-                 user_twilio_string):
+                 user_twilio_string, gmail_list):
     # Check if user intend to search in all pincodes of the entered districts
     all_pincodes_flag = 0
     if len(user_pincodes) == 1 and str.lower(user_pincodes[0]) == "all":
@@ -140,10 +175,10 @@ def cowin_search(endpoint_base, districts, user_pincodes, search_date, above_18_
                     if above_18_flag and session_entry['min_age_limit'] == 18:
                         initiate_hit_handler(session_entry, user_dose, user_pincodes, all_pincodes_flag, date,
                                              attempt_reference_dict, above_18_flag, user_mobile_numbers, cowin_log_path,
-                                             user_twilio_string)
+                                             user_twilio_string, gmail_list)
                     elif above_18_flag is not True:
                         initiate_hit_handler(session_entry, user_dose, user_pincodes, all_pincodes_flag, date,
                                              attempt_reference_dict, above_18_flag, user_mobile_numbers, cowin_log_path,
-                                             user_twilio_string)
+                                             user_twilio_string, gmail_list)
 
         time.sleep(sleep)
